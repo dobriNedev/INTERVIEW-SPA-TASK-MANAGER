ccabe75c79fa022ec8bee42165d34b52ebeef989
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-01 12:26:15 +0200

    status added

--- src/components/personCard/PersonCard.js
+++ src/components/personCard/PersonCard.js
@@ -1,25 +1,41 @@
-
 import styles from './PersonCard.module.css';
+import availableLogo from './icon-available.svg';
+import busyLogo from './icon-busy.svg';
 
 const PersonCard = ({ person }) => {
     const { firstName, lastName, available, phone, email, image, location } = person;
-    const name = firstName + lastName;
+   
+    const name = `${firstName} ${lastName}`;
+    
+    let statusLogo;
+    let status;
+
+    if (available) {
+        statusLogo = availableLogo;
+        status = 'available';
+    } else {
+        statusLogo = busyLogo;
+        status = 'busy';
+    }
+
     return (
         <div className={styles.card}>
-            <div className={styles.cardInner}>
-                <img src={image} alt={name} className={styles.image} />
-                <div className={styles.info}>
+            <div className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src={image} alt={name} />
+                    <img className={styles.statusLogo} src={statusLogo} alt={status} />
+                </div>
+                <div className={styles.nameLocation}>
                     <h2 className={styles.name}>{name}</h2>
                     <p className={styles.location}>{location}</p>
-                    <span className={styles.email}>{email}</span>
-                    <span className={styles.phone}>{phone}</span>
                 </div>
+            </div>
+            <div className={styles.info}>
+                <p className={styles.phone}>phone: {phone}</p>
+                <p className={styles.email}>email: {email}</p>
             </div>
         </div>
     );
 };
 
 export default PersonCard;
-
-
-
