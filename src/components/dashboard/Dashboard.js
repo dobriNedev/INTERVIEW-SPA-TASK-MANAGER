8344b8b57533981a880dda503afb0222907ef58a
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-04-01 12:26:32 +0200

    error handling

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,11 +4,16 @@ import { PersonsContext } from '../../contexts/pesronsContext';
 import { v4 as uuidv4 } from 'uuid';
 
 import PersonCard from '../personCard/PersonCard';
+import Error from '../Error/Error';
 import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
 
-    const { persons, searchTerm } = useContext(PersonsContext);
+    const { persons, searchTerm, error } = useContext(PersonsContext);
+
+    if (error) {
+        return <Error message={error.message} />;
+    }
 
     //simple search by first name
     const displayedPersons = searchTerm
@@ -31,4 +36,3 @@ const Dashboard = () => {
 }
 
 export default Dashboard;
-
